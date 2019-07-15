c3c6f163577d9d220e39ba446ff6405faed48991
Federico De Martino <federicodm90@gmail.com>
2019-07-15 16:56:13 +0200

    exercise optional 1

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require "csv"
+set :bind, '0.0.0.0'
 
 configure :development do
   use BetterErrors::Middleware
@@ -9,5 +11,24 @@ configure :development do
 end
 
 get '/' do
-  'Hello world!'
+  @recipes = []
+  #csv_file = File.join(__dir__, '/views/recipes.csv')
+  #raise
+  csv_options = { col_sep: ',', quote_char: '"' }
+  CSV.foreach("./views/recipes.csv", csv_options) do |row|
+    @recipes << row
+  end
+  erb :index
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/new" do
+  csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+  CSV.open("./views/recipes.csv", 'ab', csv_options) do |csv|
+    csv << [params[:description], params[:name], params[:prep_time]]
+  end
+  redirect "/", 302
 end
